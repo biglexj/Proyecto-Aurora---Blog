a9223aac12e8718b8a5f8217bef528ad7322a783
biglexj <biglex.rv@gmail.com>
2024-11-26 17:37:01 -0500

    Un Poco Responsive

--- bj-producciones/yt-videos.js
+++ bj-producciones/yt-videos.js
@@ -4,7 +4,7 @@ fetch('yt-videos.json')
     return response.json();
   })
   .then(videos => {
-    // Recorremos todos los videos del JSON
+    // Renderiza las miniaturas
     videos.forEach(video => {
       video.tags.forEach(tag => {
         const videosContainer = document.querySelector(`.videos.${tag}`);
@@ -12,14 +12,16 @@ fetch('yt-videos.json')
           const videoElement = document.createElement('div');
           videoElement.classList.add('video-content');
           videoElement.innerHTML = `
-            <div class="title-wrapper"><h2 class="title-video">${video.title}</h2></div>
+            <div class="title-wrapper">
+              <h2 class="title-video">${video.title}</h2>
+            </div>
             <div class="video-box">
-              <iframe
-                src="https://www.youtube.com/embed/${video.id}"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen>
-              ></iframe>
+              <img
+                src="https://img.youtube.com/vi/${video.id}/hqdefault.jpg"
+                alt="${video.title}"
+                class="video-thumbnail"
+                data-video-id="${video.id}"
+              />
             </div>
           `;
 
@@ -27,6 +29,35 @@ fetch('yt-videos.json')
           videosContainer.appendChild(videoElement);
         }
       });
+    });
+
+    // Agrega eventos para cargar el video al hacer clic en la miniatura
+    document.addEventListener('click', event => {
+      if (event.target && event.target.classList.contains('video-thumbnail')) {
+        const videoId = event.target.getAttribute('data-video-id');
+        const videoBox = event.target.parentElement;
+
+        // Reemplaza la miniatura con el iframe y un loader
+        videoBox.innerHTML = `
+          <div class="loader"></div>
+          <iframe
+            src="https://www.youtube.com/embed/${videoId}?autoplay=1"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+          </iframe>
+        `;
+
+        // Selecciona el iframe y el loader
+        const iframe = videoBox.querySelector('iframe');
+        const loader = videoBox.querySelector('.loader');
+
+        // Añade un evento para cuando el iframe haya cargado
+        iframe.addEventListener('load', () => {
+          loader.style.display = 'none'; // Oculta el loader
+          iframe.classList.add('loaded'); // Añade la clase para la transición
+        });
+      }
     });
   })
   .catch(error => console.error('Error al cargar videos:', error));
