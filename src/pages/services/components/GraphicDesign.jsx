56ae2261d8816db1b61cd24eabff15caada14926
Biglex Dev <biglex.rv@gmail.com>
2025-03-16 18:35:49 -0500

    redesing services

--- src/pages/services/components/GraphicDesign.jsx
+++ src/pages/services/components/GraphicDesign.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ContactButtons from './ContactButtons';
+import ContactButtons from '@components/ContactButtons';
 
 const GraphicDesign = () => {
   return (
@@ -14,46 +14,42 @@ const GraphicDesign = () => {
           <div className="flex justify-center items-center w-10 h-10 mt-3 rounded-full bg-pink-panther-500 text-white">&#9658;</div>
         </div>
 
-        <div className='p-4 md:p-10 flex flex-col max-w-7xl mx-auto'>
+        <div className='px-4 sm:px-8 md:px-16 lg:px-24 mt-5 flex flex-col max-w-7xl mx-auto'>
           <div className="content-flex">
-            <div className="content">
-              <div className="content-graphic">
-                <div>
-                  <p className='text-2xl md:text-3xl'>🖼 <strong className='text-2xl md:text-3xl'>Edición y retoque fotográfico (Affinity Photo)</strong></p>
-                  <ul className='text-lg md:text-xl font-medium'>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Corrección de color y luz</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Retoque avanzado: piel, detalles y fondo</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Restauración de fotos antiguas</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Montajes fotográficos creativos</li>
-                  </ul>
-                </div>
+            <div>
+              <p className='text-2xl text-left md:text-3xl'>🖼 <strong className='text-2xl md:text-3xl'>Edición y retoque fotográfico (Affinity Photo)</strong></p>
+              <ul className='text-lg mt-2 text-left md:text-xl font-medium'>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Corrección de color y luz</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Retoque avanzado: piel, detalles y fondo</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Restauración de fotos antiguas</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Montajes fotográficos creativos</li>
+              </ul>
+            </div>
 
-                <div className='mt-5'>
-                  <p className='text-2xl md:text-3xl'>📐 <strong className='text-2xl md:text-3xl'>Diseño vectorial (Affinity Designer)</strong></p>
-                  <ul className='text-lg md:text-xl font-medium'>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Logos personalizados</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Ilustraciones vectoriales</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Diseño de íconos para apps o páginas web</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Carteles y banners publicitarios</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Diseños para impresión (stickers, camisetas)</li>
-                  </ul>
-                </div>
+            <div>
+              <p className='text-2xl text-left md:text-3xl mt-5'>📐 <strong className='text-2xl md:text-3xl'>Diseño vectorial (Affinity Designer)</strong></p>
+              <ul className='text-lg mt-2 text-left md:text-xl font-medium'>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Logos personalizados</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Ilustraciones vectoriales</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Diseño de íconos para apps o páginas web</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Carteles y banners publicitarios</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Diseños para impresión (stickers, camisetas)</li>
+              </ul>
+            </div>
 
-                <div className='mt-5'>
-                  <p className='text-2xl md:text-3xl'>📚 <strong className='text-2xl md:text-3xl'>Diseño editorial (Affinity Publisher)</strong></p>
-                  <ul className='text-lg md:text-xl font-medium'>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Folletos publicitarios</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Revistas o catálogos</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Libros y eBooks</li>
-                    <li className='ml-11'>Portada</li>
-                    <li className='ml-11'>Maquetación interna</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Currículum o portafolios profesionales</li>
-                  </ul>
-                </div>
-              </div>
+            <div>
+              <p className='text-2xl text-left md:text-3xl mt-5'>📚 <strong className='text-2xl md:text-3xl'>Diseño editorial (Affinity Publisher)</strong></p>
+              <ul className='text-lg mt-2 text-left md:text-xl font-medium'>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Folletos publicitarios</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Revistas o catálogos</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Libros y eBooks</li>
+                <li className='ml-11'>Portada</li>
+                <li className='ml-11'>Maquetación interna</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Currículum o portafolios profesionales</li>
+              </ul>
             </div>
           </div>
-          
+
           <div className="mt-8 flex justify-center">
             <ContactButtons />
           </div>
--- src/pages/services/components/VideoEditing.jsx
+++ src/pages/services/components/VideoEditing.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ContactButtons from './ContactButtons';
+import ContactButtons from '@components/ContactButtons';
 
 const VideoEditing = () => {
   return (
     <main className="video-container max-w-7xl mx-auto p-0 mt-6">
       <section className='p-4 md:p-10 bg-coral-brand-200 mt-6 rounded-2xl'>
          <div className="text-pink-panther-600 text-3xl md:text-4xl font-kefa font-bold text-center">Edición de Video</div>
-          <div className="text-base md:text-lg font-semibold text-center mt-3">
+          <div className="text-base md:text-xl font-semibold text-center mt-3">
             <h2>Tu idea, una realidad</h2>
             <p>(Usamos DaVinci Resolve como editor)</p>
           </div>
@@ -14,37 +14,33 @@ const VideoEditing = () => {
           <div className="flex justify-center items-center w-10 h-10 mt-3 rounded-full bg-pink-panther-500 text-white">&#9658;</div>
         </div>
 
-        <div className='p-4 md:p-10 flex flex-col max-w-7xl mx-auto'>
+        <div className='px-4 sm:px-8 md:px-16 lg:px-24 mt-5 flex flex-col max-w-7xl mx-auto'>
           <div className="content-flex">
-            <div className="content">
-              <div className="content-video">
-                <div>
-                  <p className='text-2xl md:text-3xl'>🎥 <strong className='text-2xl md:text-3xl'>Edición personalizada</strong></p>
-                  <ul className='text-lg md:text-xl font-medium'>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Resúmenes de eventos: Recortes dinámicos y ajustes rápidos.</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Edición de transmisiones en vivo</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Montajes rápidos: Perfectos para contenido viral.</li>
-                  </ul>
-                </div>
-                <div className='mt-5'>
-                  <p className='text-2xl md:text-3xl'>🔀 <strong className='text-2xl md:text-3xl'>Videos verticales para redes sociales</strong></p>
-                  <ul className='text-lg md:text-xl font-medium'>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Ediciones para Reels, TikTok y YouTube Shorts</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Animaciones personalizadas y transiciones impactantes.</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Optimización para formato vertical.</li>
-                    <li className='ml-11 italic'>(Precios a consultar según complejidad)</li>
-                  </ul>
-                </div>
-                <div className='mt-5'>
-                  <p className='text-2xl md:text-3xl'>🎞 <strong className='text-2xl md:text-3xl'>Servicios adicionales destacado</strong></p>
-                  <ul className='text-lg md:text-xl font-medium'>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Edición con efectos especiales: Desde transiciones hasta tracking avanzado</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Subtítulos personalizados: Perfectos para contenido en redes sociales.</li>
-                    <li className='ml-6'><i className='text-pink-panther-500'>&#9658;</i> Audio mejorado: Sincronización y limpieza de pistas.</li>
-                    <li className='ml-11 italic'>(Incluye consulta previa para definir el estilo y objetivos del proyecto)</li>
-                  </ul>
-                </div>
-              </div>
+            <div>
+              <p className='text-2xl text-left md:text-3xl'>🎥 <strong className='text-2xl md:text-3xl'>Edición personalizada</strong></p>
+              <ul className='text-lg mt-2 text-left md:text-xl font-medium'>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Resúmenes de eventos: Recortes dinámicos y ajustes rápidos.</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Edición de transmisiones en vivo</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Montajes rápidos: Perfectos para contenido viral.</li>
+              </ul>
+            </div>
+            <div>
+              <p className='text-2xl text-left md:text-3xl mt-5'>🔀 <strong className='text-2xl md:text-3xl'>Videos verticales para redes sociales</strong></p>
+              <ul className='text-lg mt-2 text-left md:text-xl font-medium'>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Ediciones para Reels, TikTok y YouTube Shorts</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Animaciones personalizadas y transiciones impactantes.</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Optimización para formato vertical.</li>
+                <li className='ml-11 italic'>(Precios a consultar según complejidad)</li>
+              </ul>
+            </div>
+            <div>
+              <p className='text-2xl text-left md:text-3xl mt-5'>🎞 <strong className='text-2xl md:text-3xl'>Servicios adicionales destacado</strong></p>
+              <ul className='text-lg mt-2 text-left md:text-xl font-medium'>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Edición con efectos especiales: Desde transiciones hasta tracking avanzado</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Subtítulos personalizados: Perfectos para contenido en redes sociales.</li>
+                <li className='ml-8'><i className='text-pink-panther-500'>&#9658;</i> Audio mejorado: Sincronización y limpieza de pistas.</li>
+                <li className='ml-11 italic'>(Incluye consulta previa para definir el estilo y objetivos del proyecto)</li>
+              </ul>
             </div>
           </div>
           
